879a9cd6401cff0e8e6486136f8dc5acafe1f00c
hhas <hhas@users.sourceforge.net>
2019-04-16 13:25:42 +0100

    fixed bugs (forgot to add `AEHandler.swift` to target)

--- SwiftAutomation/AEHandler.swift
+++ SwiftAutomation/AEHandler.swift
@@ -20,16 +20,16 @@ private let genericEventHandlerUPP: AEEventHandlerUPP = { (request, reply, refco
             AEPutParamDesc(&reply, keyAEResult, &result)
         }
     } catch let e as AutomationError { // TO DO: decide how best to implement application error reporting (standard errors - e.g. 'coercion failed', 'object not found' - might be provided as enum [this would also take any necessary message, failed object, params]; this would be based on standard 'AppleEventError' protocol, allowing apps to define their own error structs/classes should they need to report custom errors as well)
-        reply.packFixedSizeParameter(keyErrorNumber, Int32(e.code), typeSInt32) // TO DO: move this implementation onto AEDesc and have it accept Int for ease of use? (error codes should be OSStatus, aka Int32, so should still pack as typeSInt32; Q. if code is out of 32-bit range, pack as typeSInt64? or throw/log console warning and return 32-bit error code? apart from anything else, AppleScript can't handle 64-bit numbers so returning an out-of-range code will break that)
+        try! reply.packFixedSizeParameter(keyErrorNumber, value: Int32(e.code), as: typeSInt32) // TO DO: move this implementation onto AEDesc and have it accept Int for ease of use? (error codes should be OSStatus, aka Int32, so should still pack as typeSInt32; Q. if code is out of 32-bit range, pack as typeSInt64? or throw/log console warning and return 32-bit error code? apart from anything else, AppleScript can't handle 64-bit numbers so returning an out-of-range code will break that)
     } catch {
-        reply.packFixedSizeParameter(keyErrorNumber, Int32(error._code), typeSInt32)
+        try! reply.packFixedSizeParameter(keyErrorNumber, value: Int32(error._code), as: typeSInt32)
     }
     return 0 // TO DO: when to return non-zero code?
 }
 
 public func installEventHandler(eventClass: AEEventClass, eventID: AEEventID, callback: @escaping AppleEventHandler) throws {
     var callback = callback
-    try throwing(AEInstallEventHandler(eventClass, eventID, genericEventHandlerUPP, &callback, false))
+    try throwIfError(AEInstallEventHandler(eventClass, eventID, genericEventHandlerUPP, &callback, false))
 }
 
 public func removeEventHandler(eventClass: AEEventClass, eventID: AEEventID) {
